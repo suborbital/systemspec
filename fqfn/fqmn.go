ca1546e538dc359a8ab172531e9605db142955f7
Oscar Spencer <oscar@grain-lang.org>
2022-07-14 15:48:29 -0400

    feat!: FQFN -> FQMN (#3)

---
+++ fqfn/fqmn.go
@@ -0,0 +1,204 @@
+package fqmn
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+////////////////////////////////////////////////////////////////////////////////////
+// An FQMN (fully-qualified module name) is a "globally unique"
+// name for a specific module from a specific application ref
+// example: fqmn://suborbital.acmeco/api-users/add-user@98qhrfgo3089hafrouhqf48
+// i.e. fqmn://<tenant>/<namespace>/<modname>@<ref>
+//
+// Namespaces can be deeply nested.
+//
+// These URI forms are also supported:
+//
+//      /name/<namespace>/<modname>
+// 		e.g. /name/api/users/add-user
+// 		(addressing a module by name)
+//
+// 		/ref/f0e4c2f76c58916ec258f246851be
+//      e.g. /ref/<ref>
+// 		(addressing a module by module hash)
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+// NamespaceDefault and others represent conts for namespaces.
+const (
+	NamespaceDefault = "default"
+)
+
+// FQMN is a parsed fqmn.
+type FQMN struct {
+	Tenant    string `json:"tenant"`
+	Namespace string `json:"namespace"`
+	Module    string `json:"module"`
+	Ref       string `json:"ref"`
+}
+
+var ErrFQMNParseFailure = errors.New("FQMN failed to parse")
+
+var errWrongPrefix = errors.Wrap(ErrFQMNParseFailure, "FQMN must begin with 'fqmn://', '/name', or '/ref'")
+var errMustBeFullyQualified = errors.Wrap(ErrFQMNParseFailure, "FQMN text format must contain an tenant, ref, namespace, and module name")
+var errTooFewParts = errors.Wrap(ErrFQMNParseFailure, "FQMN must contain a namespace and module name")
+var errMalformedRef = errors.Wrap(ErrFQMNParseFailure, "'/ref' format may only contain one reference")
+var errTrailingSlash = errors.Wrap(ErrFQMNParseFailure, "FQMN must not end in a trailing slash")
+
+func Parse(fqmnString string) (FQMN, error) {
+	if strings.HasPrefix(fqmnString, "fqmn://") {
+		return parseTextFormat(fqmnString)
+	}
+
+	if strings.HasPrefix(fqmnString, "/name/") {
+		return parseNameUri(fqmnString)
+	}
+
+	if strings.HasPrefix(fqmnString, "/ref/") {
+		return parseRefUri(fqmnString)
+	}
+
+	return FQMN{}, errWrongPrefix
+}
+
+func parseTextFormat(fqmnString string) (FQMN, error) {
+	fqmnString = strings.TrimPrefix(fqmnString, "fqmn://")
+
+	refSegments := strings.SplitN(fqmnString, "@", 2)
+	var ref string
+	if len(refSegments) == 2 {
+		ref = refSegments[1]
+	}
+
+	fqmnString = refSegments[0]
+
+	segments := strings.Split(fqmnString, "/")
+
+	// There should be at least three segments representing the tenant, namespace, and modname.
+	// Additional segments would be the result of multi-level namespaces.
+	if len(segments) < 3 {
+		return FQMN{}, errMustBeFullyQualified
+	}
+
+	// If the last segment is empty, there was a trailing slash
+	if segments[len(segments)-1] == "" {
+		return FQMN{}, errTrailingSlash
+	}
+
+	tenant := segments[0]
+
+	// Reconstruct the namespace
+	namespace := strings.Join(segments[1:len(segments)-1], "/")
+
+	// The module name is just the last element
+	module := segments[len(segments)-1]
+
+	fqmn := FQMN{
+		Tenant:    tenant,
+		Namespace: namespace,
+		Module:    module,
+		Ref:       ref,
+	}
+
+	return fqmn, nil
+}
+
+func parseNameUri(fqmnString string) (FQMN, error) {
+	fqmnString = strings.TrimPrefix(fqmnString, "/name/")
+	segments := strings.Split(fqmnString, "/")
+
+	// There should be at least two segments
+	if len(segments) < 2 {
+		return FQMN{}, errTooFewParts
+	}
+
+	// If the last segment is empty, there was a trailing slash
+	if segments[len(segments)-1] == "" {
+		return FQMN{}, errTrailingSlash
+	}
+
+	// Reconstruct the namespace
+	namespace := strings.Join(segments[:len(segments)-1], "/")
+
+	// The function name is just the last element
+	module := segments[len(segments)-1]
+
+	fqmn := FQMN{
+		Namespace: namespace,
+		Module:    module,
+	}
+
+	return fqmn, nil
+}
+
+func parseRefUri(fqmnString string) (FQMN, error) {
+	fqmnString = strings.TrimPrefix(fqmnString, "/ref/")
+	segments := strings.Split(fqmnString, "/")
+
+	// If the last segment is empty, there was a trailing slash
+	if segments[len(segments)-1] == "" {
+		return FQMN{}, errTrailingSlash
+	}
+
+	// There should be only one segment
+	if len(segments) != 1 {
+		return FQMN{}, errMalformedRef
+	}
+
+	ref := segments[0]
+
+	fqmn := FQMN{
+		Ref: ref,
+	}
+
+	return fqmn, nil
+}
+
+func MigrateV1ToV2(name, ref string) (FQMN, error) {
+	// Parse V1 format and swap version for ref
+
+	// if the name contains a #, parse that out as the tenant.
+	tenant := ""
+	tenantParts := strings.SplitN(name, "#", 2)
+	if len(tenantParts) == 2 {
+		tenant = tenantParts[0]
+		name = tenantParts[1]
+	}
+
+	// if a Runnable is referenced with its namespace, i.e. users#getUser
+	// then we need to parse that and ensure we only match that namespace.
+
+	namespace := NamespaceDefault
+	namespaceParts := strings.SplitN(name, "::", 2)
+	if len(namespaceParts) == 2 {
+		namespace = namespaceParts[0]
+		name = namespaceParts[1]
+	}
+
+	// next, if the name contains an @, parse the name and ignore app version.
+	versionParts := strings.SplitN(name, "@", 2)
+	if len(versionParts) == 2 {
+		name = versionParts[0]
+	}
+
+	fqmn := FQMN{
+		Tenant:    tenant,
+		Namespace: namespace,
+		Module:    name,
+		Ref:       ref,
+	}
+
+	return fqmn, nil
+}
+
+// HeadlessURLPath returns the headless URL path for a function.
+func (f FQMN) HeadlessURLPath() string {
+	return fmt.Sprintf("/%s/%s/%s/%s", f.Tenant, f.Namespace, f.Module, f.Ref)
+}
+
+func FromParts(tenant, namespace, module, ref string) string {
+	return fmt.Sprintf("fqmn://%s/%s/%s/%s", tenant, ref, namespace, module)
+}
---
+++ fqfn/fqmn_test.go
@@ -0,0 +1,125 @@
+package fqmn
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type FQFNSuite struct {
+	suite.Suite
+}
+
+func TestFQFNSuite(t *testing.T) {
+	suite.Run(t, &FQFNSuite{})
+}
+
+func (s *FQFNSuite) TestParse() {
+	for _, tt := range []struct {
+		name string
+		text string
+		fqmn FQMN
+		error
+	}{
+		{"text format single-level namespace", "fqmn://suborbital.acmeco/api-users/add-user", FQMN{
+			Tenant:    "suborbital.acmeco",
+			Namespace: "api-users",
+			Module:    "add-user",
+		}, nil},
+		{"text format two-level namespace", "fqmn://suborbital.acmeco/api/users/add-user", FQMN{
+			Tenant:    "suborbital.acmeco",
+			Namespace: "api/users",
+			Module:    "add-user",
+		}, nil},
+		{"text format multi-level namespace", "fqmn://suborbital.acmeco/api/users/auora/add-user", FQMN{
+			Tenant:    "suborbital.acmeco",
+			Namespace: "api/users/auora",
+			Module:    "add-user",
+		}, nil},
+		{"text format single-level namespace with ref", "fqmn://suborbital.acmeco/api-users/add-user@98qhrfgo3089hafrouhqf48", FQMN{
+			Tenant:    "suborbital.acmeco",
+			Namespace: "api-users",
+			Module:    "add-user",
+			Ref:       "98qhrfgo3089hafrouhqf48",
+		}, nil},
+		{"text format two-level namespace with ref", "fqmn://suborbital.acmeco/api/users/add-user@98qhrfgo3089hafrouhqf48", FQMN{
+			Tenant:    "suborbital.acmeco",
+			Namespace: "api/users",
+			Module:    "add-user",
+			Ref:       "98qhrfgo3089hafrouhqf48",
+		}, nil},
+		{"text format multi-level namespace with ref", "fqmn://suborbital.acmeco/api/users/auora/add-user@98qhrfgo3089hafrouhqf48", FQMN{
+			Tenant:    "suborbital.acmeco",
+			Namespace: "api/users/auora",
+			Module:    "add-user",
+			Ref:       "98qhrfgo3089hafrouhqf48",
+		}, nil},
+		{"module by name with single-level namespace", "/name/api-users/add-user", FQMN{
+			Namespace: "api-users",
+			Module:    "add-user",
+		}, nil},
+		{"module by name with two-level namespace", "/name/api/users/add-user", FQMN{
+			Namespace: "api/users",
+			Module:    "add-user",
+		}, nil},
+		{"module by name with multi-level namespace", "/name/api/users/auora/add-user", FQMN{
+			Namespace: "api/users/auora",
+			Module:    "add-user",
+		}, nil},
+		{"by reference", "/ref/98qhrfgo3089hafrouhqf48", FQMN{
+			Ref: "98qhrfgo3089hafrouhqf48",
+		}, nil},
+		{"malformed—doesn't start with right prefix 1", "fqmn:suborbital.acmeco/api/users/auora/add-user@98qhrfgo3089hafrouhqf48", FQMN{}, errWrongPrefix},
+		{"malformed—doesn't start with right prefix 2", "https://suborbital.acmeco/api/users/auora/add-user@98qhrfgo3089hafrouhqf48", FQMN{}, errWrongPrefix},
+		{"malformed—doesn't start with right prefix 3", "/module/api/users/auora/add-user", FQMN{}, errWrongPrefix},
+		{"malformed—doesn't start with right prefix 4", "/reference/98qhrfgo3089hafrouhqf48", FQMN{}, errWrongPrefix},
+		{"malformed—not fully-qualified 1", "fqmn://suborbital.acmeco@98qhrfgo3089hafrouhqf48", FQMN{}, errMustBeFullyQualified},
+		{"malformed—not fully-qualified 2", "fqmn://suborbital.acmeco/add-user@98qhrfgo3089hafrouhqf48", FQMN{}, errMustBeFullyQualified},
+		{"malformed—not enough parts 1", "/name/add-user", FQMN{}, errTooFewParts},
+		{"malformed—not enough parts 2", "/name/suborbital.acmeco", FQMN{}, errTooFewParts},
+		{"malformed—not a reference", "/ref/98qhrfgo3089hafrouhqf48/add-user", FQMN{}, errMalformedRef},
+		{"malformed—not a reference", "/ref/98qhrfgo3089hafrouhqf48/suborbital.acmeco", FQMN{}, errMalformedRef},
+		{"malformed—not a reference", "/ref/98qhrfgo3089hafrouhqf48/suborbital.acmeco/add-user", FQMN{}, errMalformedRef},
+		{"malformed—trailing slash 1", "fqmn://suborbital.acmeco/98qhrfgo3089hafrouhqf48/api/users/add-user/", FQMN{}, errTrailingSlash},
+		{"malformed—trailing slash 2", "/ref/98qhrfgo3089hafrouhqf48/", FQMN{}, errTrailingSlash},
+	} {
+		s.Run(tt.name, func() {
+			fqmn, err := Parse(tt.text)
+
+			if err != nil {
+				s.Assertions.ErrorIs(err, tt.error)
+				return
+			}
+
+			s.Assertions.Equal(tt.fqmn, fqmn)
+		})
+	}
+}
+
+func (s *FQFNSuite) TestMigrateV1ToV2() {
+	for _, tt := range []struct {
+		name string
+		text string
+		hash string
+		fqmn FQMN
+		error
+	}{
+		{"migrates v1 to v2", "suborbital.test#default::get-file@v0.0.1", "98qhrfgo3089hafrouhqf48", FQMN{
+			Tenant:    "suborbital.test",
+			Namespace: "default",
+			Module:    "get-file",
+			Ref:       "98qhrfgo3089hafrouhqf48",
+		}, nil},
+	} {
+		s.Run(tt.name, func() {
+			fqmn, err := MigrateV1ToV2(tt.text, tt.hash)
+
+			if err != nil {
+				s.Assertions.ErrorIs(err, tt.error)
+				return
+			}
+
+			s.Assertions.Equal(fqmn, tt.fqmn)
+		})
+	}
+}
